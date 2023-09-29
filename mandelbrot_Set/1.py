5da200eee7e2a77399e9f34cd1067c5701c06eb5
Dely Silva <111365908+delysilva@users.noreply.github.com>
2023-09-29 18:13:32 -0300

    Update 1.py

--- mandelbrot_Set/1.py
+++ mandelbrot_Set/1.py
@@ -1,7 +1,9 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
-# função que retorna o número de iterações para um determinado ponto c
+from matplotlib.backend_bases import MouseButton
+
+
 def mandelbrot(c, max_iter):
     z = c
     # itera até que z "escape" (tenha a possibilidade de divergir)
@@ -15,8 +17,9 @@ def mandelbrot(c, max_iter):
     # se o ponto c pertencer ao conjunto de Mandelbrot, retorna o número máximo de iterações
     return max_iter
 
-
 # função que gera o conjunto de Mandelbrot (parâmetros: largura {referente ao eixo x}, altura {referente ao eixo y}
+
+
 def gerar_conjunto_mandelbrot(largura, altura, xmin, xmax, ymin, ymax, max_iter):
     # cria uma matriz de zeros com as dimensões da imagem
     imagem = np.zeros((altura, largura))
@@ -42,17 +45,58 @@ def gerar_conjunto_mandelbrot(largura, altura, xmin, xmax, ymin, ymax, max_iter)
 
     return imagem
 
-# Definindo os parâmetros
+
 largura = 800
 altura = 800
-xmin, xmax, ymin, ymax = -2.0, 1.0, -1.5, 1.5
 max_iter = 256
 
-# Gerando o conjunto de Mandelbrot
-imagem = gerar_conjunto_mandelbrot(largura, altura, xmin, xmax, ymin, ymax, max_iter)
+xmin, xmax, ymin, ymax = -2.0, 2.0, -2.0, 2.0
 
-# Exibindo a imagem
-plt.imshow(imagem, extent=(xmin, xmax, ymin, ymax), cmap='hot')
-plt.colorbar()
-plt.title("Conjunto de Mandelbrot")
+
+def update(xmin, xmax, ymin, ymax):
+    """
+    Gera a imagem com base no novo intervalo
+    """
+    global largura, altura, max_iter
+    img = gerar_conjunto_mandelbrot(
+        largura, altura, xmin, xmax, ymin, ymax, max_iter)
+    plt.imshow(img, cmap='hot', extent=(xmin, xmax, ymin, ymax))
+    plt.draw()
+
+
+def on_click(event):
+    """
+    Defini o novo intervalo com base na posição do click
+    """
+    global xmin, xmax, ymin, ymax
+
+    if event.button is MouseButton.LEFT:
+        print('Dando Zoom no conjunto de Mandelbrot')
+        deltaX = (xmax - xmin) / 3
+        deltaY = (ymax - ymin) / 3
+        xmin = event.xdata - deltaX
+        xmax = event.xdata + deltaX
+        ymin = event.ydata + deltaY
+        ymax = event.ydata - deltaY
+        print("Novo intervalo:", xmin, xmax, ymin, ymax)
+        update(xmin, xmax, ymin, ymax)
+    elif event.button is MouseButton.RIGHT:
+        print('Voltando ao conjunto de Mandelbrot original')
+        update(-2.0, 2.0, -2.0, 2.0)
+    else:
+        deltaX = (xmax - xmin) / 2 * 0.8
+        deltaY = (ymax - ymin) / 2 * 0.8
+        xmin = -1.5 - deltaX
+        xmax = -1.5 + deltaX
+        ymin = 0 - deltaY
+        ymax = 0 + deltaY
+        print("xmin, xmax, ymin, ymax = %lf, %lf, %lf, %lf", xmin, xmax, ymin, ymax)
+        update(xmin, xmax, ymin, ymax)
+
+
+plt.connect('button_press_event', on_click)
+# Gera a imagem inicial
+img = gerar_conjunto_mandelbrot(
+    largura, altura, xmin, xmax, ymin, ymax, max_iter)
+plt.imshow(img, cmap='hot', extent=(xmin, xmax, ymin, ymax))
 plt.show()
